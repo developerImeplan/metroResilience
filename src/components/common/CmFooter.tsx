59d0cfc2eeaf66d7fc8d4266325ba36b58f37667
J. Manuel López <35846819+JalilManuel23@users.noreply.github.com>
2024-12-31 17:55:32 -0600

    fix(CmFooter): update responsive

--- src/components/common/CmFooter.tsx
+++ src/components/common/CmFooter.tsx
@@ -7,7 +7,7 @@ import { CmImage } from "./CmImage";
 export const CmFooter = () => {
   return(
     <div className="w-full bg-[#31365c] sm:px-10 pt-10">
-      <div className="flex sm:flex-row flex-col sm:items-center sm:space-x-5">
+      <div className="flex sm:flex-row flex-col items-center sm:space-x-5">
         <div className="space-y-4 sm:w-2/3 sm:border-r-2 border-white">
           <div className="flex sm:flex-row flex-col sm:justify-center sm:space-x-10">
             <Link to="https://european-union.europa.eu/index_es">
@@ -21,11 +21,11 @@ export const CmFooter = () => {
             </Link>
           </div>
         </div>
-        <div className="flex justify-around sm:w-1/3 sm:pl-20">
+        <div className="flex justify-around sm:w-1/3 sm:pl-20 sm:space-x-0 space-x-5">
           {CM_ICONS_SN.map(i => <CmIconSN {...i} />)}
         </div>
       </div>
-      <p className="text-center text-xs text-white font-thin sm:mb-0 mb-5 py-10 px-20">
+      <p className="text-center text-xs text-white font-thin sm:mb-0 mb-5 py-10 sm:px-20 px-10">
         Este sitio web se ha realizado con la ayuda financiera de la Unión Europea. El contenido de éste es responsabilidad exclusiva del Instituto de Planeación y Gestión del Desarrollo del Área Metropolitana de Guadalajara (IMEPLAN) y bajo ninguna circunstancia debe considerarse que refleja la posición de la Unión Europea
       </p>
     </div>
